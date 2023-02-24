c6f5ed6df5cc29ac31e1ac322e510e1d83b6798f
Tsunami43 <n3m0_22@mail.ru>
2023-02-24 17:34:37 +0300

    feat(cli): update router for new commands

--- apps/shanghaictl/lib/shanghaictl.ex
+++ apps/shanghaictl/lib/shanghaictl.ex
@@ -19,6 +19,11 @@ defmodule Shanghaictl do
   defp parse_args(["help"]), do: :help
   defp parse_args(["version"]), do: :version
   defp parse_args(["status" | opts]), do: {:status, opts}
+  defp parse_args(["replicas" | opts]), do: {:replicas, opts}
+  defp parse_args(["metrics" | opts]), do: {:metrics, opts}
+  defp parse_args(["node", "join" | opts]), do: {:node_join, opts}
+  defp parse_args(["node", "leave" | opts]), do: {:node_leave, opts}
+  defp parse_args(["shutdown" | opts]), do: {:shutdown, opts}
   defp parse_args(args), do: {:unknown, args}
 
   defp execute(:help) do
@@ -29,9 +34,14 @@ defmodule Shanghaictl do
       shanghaictl <command> [options]
 
     Commands:
-      help          Show this help message
-      version       Show version information
-      status        Show cluster status and node health
+      help              Show this help message
+      version           Show version information
+      status            Show cluster status and node health
+      replicas          Show replication groups and their status
+      metrics           Show performance and operational metrics
+      node join <id>    Add a node to the cluster
+      node leave <id>   Remove a node from the cluster
+      shutdown          Safely shutdown a node
 
     For more information, see the documentation.
     """)
@@ -39,6 +49,26 @@ defmodule Shanghaictl do
 
   defp execute({:status, opts}) do
     Shanghaictl.Commands.Status.run(opts)
+  end
+
+  defp execute({:replicas, opts}) do
+    Shanghaictl.Commands.Replicas.run(opts)
+  end
+
+  defp execute({:metrics, opts}) do
+    Shanghaictl.Commands.Metrics.run(opts)
+  end
+
+  defp execute({:node_join, opts}) do
+    Shanghaictl.Commands.Node.join(opts)
+  end
+
+  defp execute({:node_leave, opts}) do
+    Shanghaictl.Commands.Node.leave(opts)
+  end
+
+  defp execute({:shutdown, opts}) do
+    Shanghaictl.Commands.Shutdown.run(opts)
   end
 
   defp execute(:version) do
