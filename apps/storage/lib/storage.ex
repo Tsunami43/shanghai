f02e03a958b31ab4e2a94e0594964ce726086387
Tsunami43 <n3m0_22@mail.ru>
2023-04-06 10:16:06 +0300

    feat(storage): add a public facade (append/read/info)

--- apps/storage/lib/storage.ex
+++ apps/storage/lib/storage.ex
@@ -1,18 +1,36 @@
 defmodule Storage do
   @moduledoc """
-  Documentation for `Storage`.
+  Public facade for the storage subsystem (the Write-Ahead Log).
+
+  Thin delegation to the WAL processes plus a small runtime summary. The full
+  stack (`Writer`/`Reader`) is available when `:storage` is configured with a
+  `data_root`; the segment `Registry`/`SegmentManager` are always running.
   """
 
+  alias Storage.WAL.{Reader, SegmentManager, Writer}
+
+  @doc "Appends data to the WAL. Requires the WAL `Writer` to be running."
+  @spec append(term()) :: {:ok, non_neg_integer()} | {:error, term()}
+  defdelegate append(data), to: Writer
+
+  @doc "Reads a log entry by LSN. Requires the WAL `Reader` to be running."
+  @spec read(non_neg_integer()) :: {:ok, term()} | {:error, term()}
+  defdelegate read(lsn), to: Reader
+
   @doc """
-  Hello world.
+  Returns a runtime summary of the storage subsystem.
 
   ## Examples
 
-      iex> Storage.hello()
-      :world
-
+      iex> info = Storage.info()
+      iex> is_boolean(info.wal_running) and is_integer(info.active_segments)
+      true
   """
-  def hello do
-    :world
+  @spec info() :: %{wal_running: boolean(), active_segments: non_neg_integer()}
+  def info do
+    %{
+      wal_running: is_pid(Process.whereis(Writer)),
+      active_segments: SegmentManager.count()
+    }
   end
 end
