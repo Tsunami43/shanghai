d3b0fe96f6207dee422c48db80dedd9899beae43
Tsunami43 <n3m0_22@mail.ru>
2023-04-07 22:09:51 +0300

    feat(query): public key/value API with telemetry and consistency

--- apps/query/lib/query.ex
+++ apps/query/lib/query.ex
@@ -40,14 +40,13 @@ defmodule Query do
       {:error, :not_found}
   """
   @spec read(String.t(), keyword()) :: {:ok, term()} | {:error, term()}
-  def read(_key, opts \\ []) do
-    _consistency = Keyword.get(opts, :consistency, ConsistencyLevel.default())
-    _timeout = Keyword.get(opts, :timeout, 5000)
-
-    # Delegate to Query.Executor.QueryExecutor
-    # Route to appropriate node based on partition key
-    # Apply consistency semantics
-    {:ok, nil}
+  def read(key, opts \\ []) do
+    with :ok <- validate_consistency(opts) do
+      # Consistency-aware routing to remote replicas is layered on top of the
+      # local store in the replication phase; today reads resolve locally,
+      # served from the read-through cache when possible.
+      measure(:read, fn -> cached_read(key) end)
+    end
   end
 
   @doc """
@@ -64,14 +63,16 @@ defmodule Query do
       {:ok, :written}
   """
   @spec write(String.t(), term(), keyword()) :: {:ok, :written} | {:error, term()}
-  def write(_key, _value, opts \\ []) do
-    _consistency = Keyword.get(opts, :consistency, ConsistencyLevel.default())
-    _timeout = Keyword.get(opts, :timeout, 5000)
-
-    # Delegate to Query.Executor.WriteExecutor
-    # Replicate based on consistency level
-    # Handle write conflicts
-    {:ok, :written}
+  def write(key, value, opts \\ []) do
+    with :ok <- validate_consistency(opts) do
+      # Replication of the write to peer nodes happens in the replication phase;
+      # the local store provides durability via the WAL.
+      measure(:write, fn ->
+        result = Query.Store.put(key, value)
+        Query.Cache.invalidate(key)
+        result
+      end)
+    end
   end
 
   @doc """
@@ -85,12 +86,16 @@ defmodule Query do
       ...> ])
       {:ok, :committed}
   """
-  @spec transact([{:read | :write, String.t(), term()}]) ::
+  @spec transact([{:write | :delete, String.t(), term()} | {:delete, String.t()}]) ::
           {:ok, :committed} | {:error, term()}
   def transact(operations) when is_list(operations) do
-    # Delegate to Query.Transaction.Coordinator
-    # Implement 2PC or optimistic concurrency control
-    {:ok, :committed}
+    # A transaction is persisted as a single WAL record, making it atomic on a
+    # single node. Cross-node 2PC is introduced in the transactions phase.
+    measure(:transact, fn ->
+      result = Query.Store.transact(operations)
+      if match?({:ok, _}, result), do: invalidate_ops(operations)
+      result
+    end)
   end
 
   @doc """
@@ -102,11 +107,204 @@ defmodule Query do
       {:ok, :deleted}
   """
   @spec delete(String.t(), keyword()) :: {:ok, :deleted} | {:error, term()}
-  def delete(_key, opts \\ []) do
-    _timeout = Keyword.get(opts, :timeout, 5000)
+  def delete(key, _opts \\ []) do
+    # Persisted as a tombstone in the WAL; compaction reclaims the space later.
+    measure(:delete, fn ->
+      result = Query.Store.delete(key)
+      Query.Cache.invalidate(key)
+      result
+    end)
+  end
 
-    # Implement as tombstone write
-    # Trigger compaction to reclaim space
-    {:ok, :deleted}
+  @doc """
+  Atomically reads and removes `key` (a pop), returning `{:ok, value}` or
+  `{:error, :not_found}`. Useful for queue/work-stealing patterns.
+
+  ## Examples
+
+      iex> Query.write("job:1", %{task: :send})
+      iex> Query.take("job:1")
+      {:ok, %{task: :send}}
+  """
+  @spec take(String.t()) :: {:ok, term()} | {:error, :not_found}
+  def take(key) do
+    measure(:take, fn ->
+      result = Query.Store.take(key)
+      if match?({:ok, _}, result), do: Query.Cache.invalidate(key)
+      result
+    end)
+  end
+
+  @doc """
+  Atomic compare-and-swap for optimistic concurrency.
+
+  Writes `new` only if the current value equals `expected`; pass `:absent` to
+  write only when the key does not exist. Returns `{:ok, :swapped}` or
+  `{:error, :precondition_failed}`.
+
+  ## Examples
+
+      iex> Query.cas("counter", :absent, 1)
+      {:ok, :swapped}
+
+      iex> Query.cas("counter", 1, 2)
+      {:ok, :swapped}
+  """
+  @spec cas(String.t(), term() | :absent, term()) :: {:ok, :swapped} | {:error, term()}
+  def cas(key, expected, new) do
+    measure(:cas, fn ->
+      result = Query.Store.cas(key, expected, new)
+      if match?({:ok, _}, result), do: Query.Cache.invalidate(key)
+      result
+    end)
+  end
+
+  @doc """
+  Atomic read-modify-write. Applies `fun` to the current value (or `default`
+  when the key is absent) and stores the result, returning `{:ok, new_value}`.
+
+  ## Examples
+
+      iex> Query.update("tags", [], &["new" | &1])
+      {:ok, ["new"]}
+  """
+  @spec update(String.t(), term(), (term() -> term())) :: {:ok, term()} | {:error, term()}
+  def update(key, default, fun) when is_function(fun, 1) do
+    measure(:update, fn ->
+      result = Query.Store.update(key, default, fun)
+      if match?({:ok, _}, result), do: Query.Cache.invalidate(key)
+      result
+    end)
+  end
+
+  @doc """
+  Atomically adds `amount` (default `1`) to the numeric value at `key`,
+  treating a missing key as `0`. Returns `{:ok, new_value}`.
+
+  ## Examples
+
+      iex> Query.increment("hits")
+      {:ok, 1}
+
+      iex> Query.increment("hits", 5)
+      {:ok, 6}
+  """
+  @spec increment(String.t(), number()) :: {:ok, number()} | {:error, term()}
+  def increment(key, amount \\ 1) when is_number(amount) do
+    measure(:increment, fn ->
+      result = Query.Store.increment(key, amount)
+      if match?({:ok, _}, result), do: Query.Cache.invalidate(key)
+      result
+    end)
+  end
+
+  @doc """
+  Returns all `{key, value}` pairs whose key starts with `prefix`.
+
+  Useful for range/collection access patterns (event streams, per-entity keys).
+
+  ## Examples
+
+      iex> Query.scan("events:order-1:")
+      {:ok, [{"events:order-1:1", ...}, {"events:order-1:2", ...}]}
+  """
+  @spec scan(binary()) :: {:ok, [{binary(), term()}]}
+  def scan(prefix) when is_binary(prefix) do
+    measure(:scan, fn -> {:ok, Query.Store.scan(prefix)} end)
+  end
+
+  @doc """
+  Reads several keys at once, returning `{:ok, %{key => value}}` for the keys
+  that exist (missing keys are omitted).
+
+  ## Examples
+
+      iex> Query.mget(["a", "b", "missing"])
+      {:ok, %{"a" => 1, "b" => 2}}
+  """
+  @spec mget([String.t()]) :: {:ok, %{optional(String.t()) => term()}}
+  def mget(keys) when is_list(keys) do
+    measure(:mget, fn -> {:ok, Query.Store.mget(keys)} end)
+  end
+
+  @doc """
+  Returns a runtime summary of the query layer: the store's durability mode,
+  the number of records recovered on start, the live key count, and cache stats.
+
+  ## Examples
+
+      iex> {:ok, info} = Query.info()
+      iex> is_boolean(info.store.durable) and is_integer(info.cache.size)
+      true
+  """
+  @spec info() :: {:ok, %{store: map(), cache: map()}}
+  def info do
+    {:ok, store} = Query.Store.info()
+    {:ok, cache} = Query.Cache.stats()
+    {:ok, %{store: store, cache: cache}}
+  end
+
+  @doc "Returns every stored key."
+  @spec keys() :: [term()]
+  defdelegate keys(), to: Query.Store
+
+  @doc "Returns the number of stored keys."
+  @spec count() :: non_neg_integer()
+  defdelegate count(), to: Query.Store
+
+  # Validates the requested consistency level, if any was provided.
+  defp validate_consistency(opts) do
+    level = Keyword.get(opts, :consistency, ConsistencyLevel.default())
+
+    if ConsistencyLevel.valid?(level) do
+      :ok
+    else
+      {:error, {:invalid_consistency, level}}
+    end
+  end
+
+  # Runs `fun`, timing it and emitting a `[:shanghai, :query, :operation]`
+  # telemetry event — the query layer is observable by default.
+  defp measure(operation, fun) do
+    start = System.monotonic_time()
+    result = fun.()
+
+    duration_ms =
+      (System.monotonic_time() - start)
+      |> System.convert_time_unit(:native, :microsecond)
+      |> Kernel./(1000)
+
+    Observability.Metrics.query_operation_completed(operation, duration_ms, result_tag(result))
+    result
+  end
+
+  defp result_tag({:ok, _}), do: :ok
+  defp result_tag(_), do: :error
+
+  # Read-through cache: serve from cache on a hit, otherwise fetch from the
+  # store and populate the cache on success.
+  defp cached_read(key) do
+    case Query.Cache.get(key) do
+      {:ok, value} ->
+        {:ok, value}
+
+      :miss ->
+        case Query.Store.get(key) do
+          {:ok, value} = ok ->
+            Query.Cache.put(key, value)
+            ok
+
+          other ->
+            other
+        end
+    end
+  end
+
+  defp invalidate_ops(operations) do
+    Enum.each(operations, fn
+      {:write, key, _value} -> Query.Cache.invalidate(key)
+      {:delete, key} -> Query.Cache.invalidate(key)
+      _ -> :ok
+    end)
   end
 end
