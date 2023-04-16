c8765fd361f81ff6054cd9f6592ab49e9d08e1d4
Tsunami43 <n3m0_22@mail.ru>
2023-04-16 13:23:48 +0300

    feat(obs): public facade for telemetry and logging

--- apps/observability/lib/observability.ex
+++ apps/observability/lib/observability.ex
@@ -1,18 +1,24 @@
 defmodule Observability do
   @moduledoc """
-  Documentation for `Observability`.
+  Public facade for the observability subsystem: telemetry, metrics and
+  structured logging.
   """
 
+  alias Observability.Logger, as: StructuredLogger
+  alias Observability.Metrics
+
   @doc """
-  Hello world.
+  Returns the list of telemetry event names Shanghai emits.
 
   ## Examples
 
-      iex> Observability.hello()
-      :world
-
+      iex> [:shanghai, :query, :operation] in Observability.event_names()
+      true
   """
-  def hello do
-    :world
-  end
+  @spec event_names() :: [[atom()]]
+  defdelegate event_names(), to: Metrics
+
+  @doc "Generates a new correlation id for request tracing."
+  @spec new_correlation_id() :: String.t()
+  defdelegate new_correlation_id(), to: StructuredLogger
 end
