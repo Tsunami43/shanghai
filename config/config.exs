0ae6ecfb94dfad3cfd019b0f002cbb009e287730
Tsunami43 <n3m0_22@mail.ru>
2023-04-24 11:07:20 +0300

    fix(config): resolve env config relative to the config file

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,10 @@
 import Config
 
-# Import environment specific config
-if File.exists?("config/#{config_env()}.exs") do
+# Import environment specific config.
+#
+# Resolve the path relative to THIS file (not the current working directory) so
+# the env config is loaded reliably whether Mix runs from the umbrella root or
+# from an individual app directory (e.g. `cd apps/foo && mix test`).
+if File.exists?(Path.join(__DIR__, "#{config_env()}.exs")) do
   import_config "#{config_env()}.exs"
 end
