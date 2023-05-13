4cdbe1ed1c624ad3c38ea470921e7d96df62da4f
Tsunami43 <n3m0_22@mail.ru>
2023-05-13 17:01:18 +0300

    test(cluster): node metadata value object

---
+++ apps/cluster/test/cluster/value_objects/node_metadata_test.exs
@@ -0,0 +1,71 @@
+defmodule Cluster.ValueObjects.NodeMetadataTest do
+  use ExUnit.Case, async: true
+
+  alias Cluster.ValueObjects.NodeMetadata
+
+  doctest NodeMetadata
+
+  test "new/0 uses defaults" do
+    md = NodeMetadata.new()
+
+    assert MapSet.size(md.capabilities) == 0
+    assert md.tags == %{}
+    assert md.resources == %{}
+    assert md.version == "0.1.0"
+  end
+
+  test "new/1 accepts overrides" do
+    md = NodeMetadata.new(version: "1.2.3", tags: %{region: "eu"})
+
+    assert md.version == "1.2.3"
+    assert md.tags == %{region: "eu"}
+  end
+
+  test "capabilities can be added and queried" do
+    md = NodeMetadata.new() |> NodeMetadata.add_capability(:storage)
+
+    assert NodeMetadata.has_capability?(md, :storage)
+    refute NodeMetadata.has_capability?(md, :replication)
+  end
+
+  test "tags can be set and read with a default" do
+    md = NodeMetadata.new() |> NodeMetadata.put_tag(:zone, "a")
+
+    assert NodeMetadata.get_tag(md, :zone) == "a"
+    assert NodeMetadata.get_tag(md, :missing) == nil
+    assert NodeMetadata.get_tag(md, :missing, :fallback) == :fallback
+  end
+
+  test "resources are merged, not replaced" do
+    md =
+      NodeMetadata.new(resources: %{cpu: 4})
+      |> NodeMetadata.update_resources(%{mem: 16})
+
+    assert md.resources == %{cpu: 4, mem: 16}
+  end
+
+  test "to_map/from_map round-trips" do
+    md =
+      NodeMetadata.new(version: "9.9.9")
+      |> NodeMetadata.add_capability(:query)
+      |> NodeMetadata.put_tag(:role, "leader")
+      |> NodeMetadata.update_resources(%{disk: 100})
+
+    restored = md |> NodeMetadata.to_map() |> NodeMetadata.from_map()
+
+    assert restored == md
+  end
+
+  test "from_map accepts string keys" do
+    md =
+      NodeMetadata.from_map(%{
+        "capabilities" => [:a, :b],
+        "tags" => %{"k" => "v"},
+        "resources" => %{"cpu" => 2},
+        "version" => "2.0.0"
+      })
+
+    assert NodeMetadata.has_capability?(md, :a)
+    assert md.version == "2.0.0"
+  end
+end
