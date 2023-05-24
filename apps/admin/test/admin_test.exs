67a1066a86caebb39bbcbd5e19d1d2abc83e71c7
Tsunami43 <n3m0_22@mail.ru>
2023-05-24 15:12:00 +0300

    test(admin): aggregate health check

--- apps/admin/test/admin_test.exs
+++ apps/admin/test/admin_test.exs
@@ -1,8 +1,42 @@
 defmodule AdminTest do
-  use ExUnit.Case
-  doctest Admin
+  use ExUnit.Case, async: false
 
-  test "greets the world" do
-    assert Admin.hello() == :world
+  setup_all do
+    # The cluster app omits its `mod:` under the test env, and Replication.Monitor
+    # is disabled there, so start them explicitly for a healthy report.
+    case Cluster.Application.start(:normal, []) do
+      {:ok, _} -> :ok
+      {:error, {:already_started, _}} -> :ok
+    end
+
+    case Replication.Monitor.start_link([]) do
+      {:ok, _} -> :ok
+      {:error, {:already_started, _}} -> :ok
+    end
+
+    :ok
+  end
+
+  describe "status_of/1" do
+    test "healthy when all checks pass" do
+      assert Admin.Health.status_of(%{storage: true, cluster: true}) == :healthy
+    end
+
+    test "degraded when any check fails" do
+      assert Admin.Health.status_of(%{storage: true, cluster: false}) == :degraded
+    end
+  end
+
+  describe "health/0" do
+    test "reports every subsystem and an overall status" do
+      report = Admin.health()
+
+      assert Enum.sort(Map.keys(report.checks)) == [:cluster, :query, :replication, :storage]
+      assert report.status in [:healthy, :degraded]
+    end
+
+    test "is healthy when all subsystem processes are running" do
+      assert Admin.health().status == :healthy
+    end
   end
 end
