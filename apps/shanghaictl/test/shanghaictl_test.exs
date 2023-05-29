5630650060c07467c0b4f44872d081d495f9de0f
Tsunami43 <n3m0_22@mail.ru>
2023-05-29 12:41:23 +0300

    test(ctl): CLI argument parsing

---
+++ apps/shanghaictl/test/shanghaictl_test.exs
@@ -0,0 +1,38 @@
+defmodule ShanghaictlTest do
+  use ExUnit.Case, async: true
+
+  doctest Shanghaictl
+
+  describe "parse/1" do
+    test "no args and 'help' map to :help" do
+      assert Shanghaictl.parse([]) == :help
+      assert Shanghaictl.parse(["help"]) == :help
+    end
+
+    test "'version' maps to :version" do
+      assert Shanghaictl.parse(["version"]) == :version
+    end
+
+    test "read commands carry their options" do
+      assert Shanghaictl.parse(["status", "--format", "json"]) == {:status, ["--format", "json"]}
+      assert Shanghaictl.parse(["replicas"]) == {:replicas, []}
+      assert Shanghaictl.parse(["metrics"]) == {:metrics, []}
+    end
+
+    test "node join/leave are distinguished" do
+      assert Shanghaictl.parse(["node", "join", "n1", "--host", "h"]) ==
+               {:node_join, ["n1", "--host", "h"]}
+
+      assert Shanghaictl.parse(["node", "leave", "n1"]) == {:node_leave, ["n1"]}
+    end
+
+    test "shutdown carries its options" do
+      assert Shanghaictl.parse(["shutdown", "--graceful"]) == {:shutdown, ["--graceful"]}
+    end
+
+    test "unrecognized input is :unknown with the original args" do
+      assert Shanghaictl.parse(["bogus", "x"]) == {:unknown, ["bogus", "x"]}
+      assert Shanghaictl.parse(["node"]) == {:unknown, ["node"]}
+    end
+  end
+end
