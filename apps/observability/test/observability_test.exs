674d45377f6ad4e01153312e2dbc22b931e3f362
Tsunami43 <n3m0_22@mail.ru>
2023-06-03 21:36:15 +0300

    test(obs): cover the observability facade

--- apps/observability/test/observability_test.exs
+++ apps/observability/test/observability_test.exs
@@ -1,8 +1,19 @@
 defmodule ObservabilityTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+
   doctest Observability
 
-  test "greets the world" do
-    assert Observability.hello() == :world
+  test "event_names/0 lists the known telemetry events" do
+    names = Observability.event_names()
+
+    assert [:shanghai, :query, :operation] in names
+    assert [:shanghai, :storage, :wal, :write] in names
+  end
+
+  test "new_correlation_id/0 returns a hex string" do
+    id = Observability.new_correlation_id()
+
+    assert is_binary(id)
+    assert id =~ ~r/^[0-9a-f]+$/
   end
 end
